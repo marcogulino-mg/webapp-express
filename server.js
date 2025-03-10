1001f304f443555cef07ed5b6f1f6dff3119651e
marco-gulino <marcogulino98@gmail.com>
2025-03-10 16:14:47 +0100

    [feat] set server base configuration and create the starting route

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+// IMPORT .env
+require("dotenv").config();
 // IMPORT express
 const express = require("express");
+// Server Base Configuration
+const app = express();
+const port = process.env.PORT;
+
+// HomePage
+app.get("/", (req, res) => {
+  res.send("<h1>Rotta di Partenza</h1>");
+});
+
+//Server start (port: 3000)
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
